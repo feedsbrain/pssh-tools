2e005f8bee56257f548950900793fa61f42af67e
Indra Gunawan <feedsbrain@gmail.com>
2019-06-24 16:48:40 +0800

    Refactor Exports

--- test/pssh.js
+++ test/pssh.js
@@ -6,22 +6,20 @@ const LA_URL = 'https://test.playready.microsoft.com/service/rightsmanager.asmx'
 const PSSH_TEST = 'AAAAQXBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAACESEJjp6TNjifjKjuoDBeg+VrUaCmludGVydHJ1c3QiASo='
 
 test('Should return Widevine PSSH version 0 without KID', t => {
-  const wvTools = pssh.tools(pssh.system.WIDEVINE.name)
   const payload = { contentId: 'cenc-content-id', trackType: 'HD', provider: 'widevine_test', protectionScheme: 'cenc', dataOnly: false }
 
-  const data = wvTools.encodePssh(payload)
-  const result = wvTools.decodePssh(data)
+  const data = pssh.widevine.encodePssh(payload)
+  const result = pssh.tools.decodePssh(data)
 
   t.is(result.version, 0)
   t.is(result.keyCount, 0)
 })
 
 test('Should return Widevine PSSH version 0 with KIDs', t => {
-  const wvTools = pssh.tools(pssh.system.WIDEVINE.name)
   const payload = { contentId: 'cenc-content-id', trackType: 'HD', keyIds: [KID], provider: 'widevine_test', protectionScheme: 'cenc', dataOnly: false }
 
-  const data = wvTools.encodePssh(payload)
-  const result = wvTools.decodePssh(data)
+  const data = pssh.widevine.encodePssh(payload)
+  const result = pssh.tools.decodePssh(data)
 
   console.log(result.printPssh())
 
@@ -30,11 +28,10 @@ test('Should return Widevine PSSH version 0 with KIDs', t => {
 })
 
 test('Should return PlayReady PSSH version 1 with KID', t => {
-  const prTools = pssh.tools(pssh.system.PLAYREADY.name)
   const payload = { keyIds: [KID], licenseUrl: LA_URL, dataOnly: false }
 
-  const data = prTools.encodePssh(payload)
-  const result = prTools.decodePssh(data)
+  const data = pssh.playready.encodePssh(payload)
+  const result = pssh.tools.decodePssh(data)
 
   console.log(result.printPssh())
 
@@ -43,11 +40,10 @@ test('Should return PlayReady PSSH version 1 with KID', t => {
 })
 
 test('Should return PlayReady PSSH version 1 with Header Version 4.0.0.0 and KID', t => {
-  const prTools = pssh.tools(pssh.system.PLAYREADY.name)
   const payload = { keyIds: [KID], licenseUrl: LA_URL, compatibilityMode: true, dataOnly: false }
 
-  const data = prTools.encodePssh(payload)
-  const result = prTools.decodePssh(data)
+  const data = pssh.playready.encodePssh(payload)
+  const result = pssh.tools.decodePssh(data)
 
   console.log(result.printPssh())
 
@@ -56,9 +52,7 @@ test('Should return PlayReady PSSH version 1 with Header Version 4.0.0.0 and KID
 })
 
 test('Should be able to decode PSSH generated from PSSH-BOX', t => {
-  const wvTools = pssh.tools(pssh.system.WIDEVINE.name)
-  const result = wvTools.decodePssh(PSSH_TEST)
-
+  const result = pssh.tools.decodePssh(PSSH_TEST)
   console.log(result.printPssh())
 
   t.is(result.version, 0)
